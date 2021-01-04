bdd35f313d75a5bf3c73436b36253bdeff056fb0
Direnc <direnc99@gmail.com>
2021-01-04 13:05:35 +0100

    updated target images to label only

--- js/products/ProductImages.js
+++ js/products/ProductImages.js
@@ -1,12 +1,12 @@
 import MILK_TARGET from '../res/K-Klassic-Vollmilch.png';
-import MILK_TARGET2 from '../res/Landliebe-Milch.jpeg';
-import MILK_TARGET3 from '../res/Hafermilch.jpg';
-import BREAD_TARGET1 from '../res/Vollkornbrot.jpg';
+import MILK_TARGET2 from '../res/Landliebe-Milch.png';
+import MILK_TARGET3 from '../res/Hafermilch.png';
+import BREAD_TARGET1 from '../res/Vollkornbrot.png';
 import BREAD_TARGET2 from '../res/Weizenbrot.jpg';
 import BREAD_TARGET3 from '../res/Toastbrot.jpg';
 import CHEESE_TARGET1 from '../res/Thekenkäse.jpg';
 import CHEESE_TARGET2 from '../res/Verpacktkäse.jpg';
-import CHEESE_TARGET3 from '../res/Vegan.jpg';
+import CHEESE_TARGET3 from '../res/Veganer-Käse.png';
 
 const Images = {
   product_id1: MILK_TARGET,
