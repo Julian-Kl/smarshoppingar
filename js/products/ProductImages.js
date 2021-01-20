afb00c982752f183f3e265bc9e658ac409f57089
proohit <direnc99@gmail.com>
2021-01-20 23:55:28 +0100

    added more products

--- js/products/ProductImages.js
+++ js/products/ProductImages.js
@@ -4,10 +4,16 @@ import MILK_TARGET3 from '../res/Hafermilch.png';
 import BREAD_TARGET1 from '../res/Vollkornbrot.png';
 import BREAD_TARGET2 from '../res/Weizenbrot.jpg';
 import BREAD_TARGET3 from '../res/Toastbrot.jpg';
+import BREAD_TARGET4 from '../res/Edeka-Roggenmischbrot.jpg';
 import CHEESE_TARGET1 from '../res/Thekenkäse.jpg';
 import CHEESE_TARGET2 from '../res/Verpacktkäse.jpg';
 import CHEESE_TARGET3 from '../res/Veganer-Käse.png';
-
+import CHOCOCHIPS_TARGET1 from '../res/Edeka-Chocochips.jpg';
+import JOGHURTDRESSING_TARGET1 from '../res/Edeka-Joghurtdressing.jpg';
+import BREAD_TARGET5 from '../res/Edeka-Kornbrot.jpg';
+import FRISCHKASE_TARGET1 from '../res/Edeka-Krauterfrischkase.jpg';
+import NUGGETS_TARGET1 from '../res/Edeka-NuggetsPommes.jpg';
+import CHOCOLATE_TARGET1 from '../res/Edeka-Vollmilch-Haselnuss.jpg';
 const Images = {
   product_id1: MILK_TARGET,
   product_id2: MILK_TARGET2,
@@ -18,6 +24,13 @@ const Images = {
   product_id7: CHEESE_TARGET1,
   product_id8: CHEESE_TARGET2,
   product_id9: CHEESE_TARGET3,
+  product_id10: BREAD_TARGET4,
+  product_id11: CHOCOCHIPS_TARGET1,
+  product_id12: JOGHURTDRESSING_TARGET1,
+  product_id13: BREAD_TARGET5,
+  product_id14: FRISCHKASE_TARGET1,
+  product_id15: NUGGETS_TARGET1,
+  product_id16: CHOCOLATE_TARGET1,
 };
 
 export default Images;
