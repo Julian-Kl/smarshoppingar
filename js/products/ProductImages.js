5fee9263791a45a68e542f72646739443144c863
proohit <direnc99@gmail.com>
2021-01-27 16:18:53 +0100

    added more products

--- js/products/ProductImages.js
+++ js/products/ProductImages.js
@@ -11,9 +11,16 @@ import CHEESE_TARGET3 from '../res/Veganer-Käse.png';
 import CHOCOCHIPS_TARGET1 from '../res/Edeka-Chocochips.jpg';
 import JOGHURTDRESSING_TARGET1 from '../res/Edeka-Joghurtdressing.jpg';
 import BREAD_TARGET5 from '../res/Edeka-Kornbrot.jpg';
-import FRISCHKASE_TARGET1 from '../res/Edeka-Krauterfrischkase.jpg';
+import FRISCHKASE_TARGET1 from '../res/Miree-Paprika-Chili.jpg';
 import NUGGETS_TARGET1 from '../res/Edeka-NuggetsPommes.jpg';
-import CHOCOLATE_TARGET1 from '../res/Edeka-Vollmilch-Haselnuss.jpg';
+import EXQUISA from '../res/Exquisa.jpg';
+import JALAPENOS from '../res/kuhne-jalapenos.jpg';
+import EDEKA_LAKTOSEFREIE_MILCH_38 from '../res/Edeka-laktosefreie-milch-38.jpg';
+import EDEKA_REMOULADE from '../res/edeka-remoulade.jpg';
+import HOHES_C_FRUHSTUCK_ORANGE_KAROTTE from '../res/hohes-c-fruhstuck-karotte-orange.jpg';
+import EDEKA_JUNGER_GOUDA from '../res/EDEKA_JUNGER_GOUDA.jpg';
+import OMAS_URHEFE from '../res/OMAS_URHEFE.jpg';
+
 const Images = {
   product_id1: MILK_TARGET,
   product_id2: MILK_TARGET2,
@@ -30,7 +37,13 @@ const Images = {
   product_id13: BREAD_TARGET5,
   product_id14: FRISCHKASE_TARGET1,
   product_id15: NUGGETS_TARGET1,
-  product_id16: CHOCOLATE_TARGET1,
+  product_id16: EXQUISA,
+  product_id17: JALAPENOS,
+  product_id18: EDEKA_LAKTOSEFREIE_MILCH_38,
+  product_id19: EDEKA_REMOULADE,
+  product_id20: HOHES_C_FRUHSTUCK_ORANGE_KAROTTE,
+  product_id21: EDEKA_JUNGER_GOUDA,
+  product_id22: OMAS_URHEFE,
 };
 
 export default Images;
